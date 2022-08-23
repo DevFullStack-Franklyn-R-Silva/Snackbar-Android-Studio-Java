7890dad22ee915ec3cb047ddebabed88896cee33
Franklyn R <franklyn1998@live.com>
2022-08-23 12:37:14 -0300

    Adicionando o botao fechar no snackbar

--- app/src/main/java/com/hadesfranklyn/snackbar/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/snackbar/MainActivity.java
@@ -10,7 +10,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonAbrir;
+    private Button buttonAbrir, buttonFechar;
+    private Snackbar snackbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonAbrir = findViewById(R.id.buttonAbrir);
+        buttonFechar = findViewById(R.id.buttonFechar);
+
 
         buttonAbrir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Snackbar snackbar = Snackbar.make(
+                snackbar = Snackbar.make(
                         view,
                         "Botão pressionado",
                         Snackbar.LENGTH_INDEFINITE
@@ -33,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
 //                                buttonAbrir.setText("Botão abrir alterado!");
                     }
                 });
-
                 snackbar.show();
-
+            }
+        });
+        buttonFechar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
             }
         });
     }
