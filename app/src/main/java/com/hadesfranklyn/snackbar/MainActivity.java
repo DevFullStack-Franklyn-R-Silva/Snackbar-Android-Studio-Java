ea6faf32bbd6104a3f4a11daa59728a21983e42a
Franklyn R <franklyn1998@live.com>
2022-08-23 12:30:05 -0300

    Snackbar

--- app/src/main/java/com/hadesfranklyn/snackbar/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/snackbar/MainActivity.java
@@ -1,9 +1,12 @@
 package com.hadesfranklyn.snackbar;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar snackbar = Snackbar.make(
+                        view,
+                        "Botão pressionado",
+                        Snackbar.LENGTH_INDEFINITE
+                ).setAction("Confirmar", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+//                                buttonAbrir.setText("Botão abrir alterado!");
+                    }
+                });
+
+                snackbar.show();
+
+            }
+        });
+    }
+
+    public void abrirSnackbar(View view) {
+
     }
 }
