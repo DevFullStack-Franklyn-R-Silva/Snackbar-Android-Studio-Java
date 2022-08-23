38e7f0f675014fb53dc1baf63c55d1378f4cf838
Franklyn R <franklyn1998@live.com>
2022-08-23 12:18:20 -0300

    first commit

---
+++ app/src/main/java/com/hadesfranklyn/snackbar/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hadesfranklyn.snackbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
